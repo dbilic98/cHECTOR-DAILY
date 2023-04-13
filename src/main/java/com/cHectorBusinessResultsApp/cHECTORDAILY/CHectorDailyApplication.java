179a5e8c7e271fb3d8c501cc5b6fe96c767c8782
dbilic98 <doris.bilic@fsre.sum.ba>
2023-04-13 09:25:23 +0200

    Added some files

---
+++ src/main/java/com/cHectorBusinessResultsApp/cHECTORDAILY/CHectorDailyApplication.java
@@ -0,0 +1,13 @@
+package com.cHectorBusinessResultsApp.cHECTORDAILY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CHectorDailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CHectorDailyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cHectorBusinessResultsApp/cHECTORDAILY/CHectorDailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cHectorBusinessResultsApp.cHECTORDAILY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CHectorDailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
