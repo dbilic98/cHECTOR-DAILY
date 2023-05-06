e80801c6337f89a2dfc95c2d14048a2a56415234
dbilic98 <doris.bilic@student.fsre.ba>
2023-05-06 17:20:39 +0200

    Format file

--- src/main/java/com/cHectorBusinessResultsApp/cHECTORDAILY/CHectorDailyApplication.java
+++ src/main/java/com/cHectorBusinessResultsApp/cHECTORDAILY/CHectorDailyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CHectorDailyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CHectorDailyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CHectorDailyApplication.class, args);
+    }
 
 }
