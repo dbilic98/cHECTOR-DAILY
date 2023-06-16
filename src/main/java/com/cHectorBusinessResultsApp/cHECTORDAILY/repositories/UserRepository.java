c48bea6b958a29d7bc6c5e4b647a655a50f7476f
teaarozic <119743127+teaarozic@users.noreply.github.com>
2023-06-16 20:45:25 +0200

    Add files via upload

--- src/main/java/com/cHectorBusinessResultsApp/cHECTORDAILY/repositories/UserRepository.java
+++ src/main/java/com/cHectorBusinessResultsApp/cHECTORDAILY/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.cHectorBusinessResultsApp.cHECTORDAILY.repositories;
 
 import com.cHectorBusinessResultsApp.cHECTORDAILY.model.Korisnik;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<Korisnik, Long> {
 
     Korisnik findByUsername(String username);
